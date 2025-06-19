c81046158b188ead24def05c34828042f279e621
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-06-19 14:06:40 +0200

    Update notfound.jsx

--- src/util/notfound.jsx
+++ src/util/notfound.jsx
@@ -1,15 +1,18 @@
 
 const styles = {
-    notfound: "flex justify-center items-center h-screen text-center",
-    headText: "text-red-500",
-    standardText: "text-gray-500",
+    notfound: "flex flex-col justify-center items-center h-screen text-center relative",
+    container: "absolute top-[30%] left-[50%] transform -translate-x-[50%] -translate-y-[50%]",
+    headText: "text-red-700 text-3xl font-bold mb-3",
+    standardText: "text-[#61615f] text-base font-medium",
 }
 
 const NotFound = () => {
     return (
         <div className={styles.notfound}>
-            <h1 className={styles.headText}>404 - Page Not Found</h1>
-            <p className={styles.standardText}>The page you are looking for does not exist.</p>
+            <div className={styles.container}>
+                <h1 className={styles.headText}>404 - Page Not Found</h1>
+                <p className={styles.standardText}>The page you are looking for does not exist.</p>
+            </div>
         </div>
     );
 }
