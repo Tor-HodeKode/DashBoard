7ef1d0e028cf0ceffd0bf8e2a7512123c32f414b
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-06-19 12:14:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { base_url as base } from "./config.js";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: base, // Set the base URL for the application
   plugins: [tailwindcss(), react()],
 });
